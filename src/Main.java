c30162374d6c0d01cd1c5990b9f4aba044da388b
Lucas Rafael Avelino <125045079+LucaoRAs@users.noreply.github.com>
2024-06-23 19:20:31 -0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        Livro obj1= new Livro("Calculo I");
-        obj1.setAutor("guanabaro");
-        obj1.setAno("2020");
-        //System.out.println(obj1);
-        Livro obj2 = new Livro("ED.dados");
-        obj2.setAutor("heisenberg");
-        obj2.setAno("2021");
-        //System.out.println(obj2);
-        Livro obj3 = new Livro("Banco de Dados");
-        obj3.setAutor("Amado Batista");
-        obj3.setAno("2022");
-        //System.out.println(obj3);
+        Livro obj1 = new Livro("Java Programming");
+        obj1.setAutor("Herbert Schildt");
+        obj1.setAno("2018");
+
+        Livro obj2 = new Livro("Python for Data Analysis");
+        obj2.setAutor("Wes McKinney");
+        obj2.setAno("2017");
+
+        Livro obj3 = new Livro("Clean Code");
+        obj3.setAutor("Robert C. Martin");
+        obj3.setAno("2008");
 
         // instanciar biblioteca
         Biblioteca bib = new Biblioteca();
         bib.inserir(obj1); //inserção de cada livro na biblioteca
-        bib.inserir(obj2); //insercão de cada livro na biblioteca
+        bib.inserir(obj2); //inserção de cada livro na biblioteca
         bib.inserir(obj3); //inserção de cada livro na biblioteca
 
         bib.listarTodos(); //listar todos os livros
 
         System.out.println("Buscando livro");
         bib.BuscaLivros(2); // busca o livro pelo ID
-        //bib.Remocao(1); // remocao o livro pelo id
-        bib.listarTodos(); // listar todos os livro na bib
-        
+        //bib.Remocao(1); // remoção do livro pelo id
+        bib.listarTodos(); // listar todos os livros na biblioteca
     }
-    
 }
+
